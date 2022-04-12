bd765b920d409d5166f34e0e1b343e2c08b978d8
cheikabbas <cheikabbas@gmail.com>
2022-04-12 11:26:45 +0000

    First commit

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,14 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=36)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
 
 
 class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     published = models.BooleanField(default=False)
-    date = models.DateField(blank=True)
+    date = models.DateField(blank=True, null=True)
     content = models.TextField()
     description = models.TextField()
 
@@ -39,7 +55,7 @@ class Book(models.Model):
         (SCIENCE_FICTION, "Science-fiction"),
     ]
     title = models.CharField(max_length=100)
-    price = models.DecimalField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
     summary = models.TextField(blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True)
     category = models.CharField(max_length=30, blank=True, choices=GENRES)
